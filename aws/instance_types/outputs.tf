26e7e756dc1b5c7eb59f7edf647cbb3876c89554
Derek Smith <dsmith@dsmith-macbookpro.local>
2020-11-20 20:27:46 -0800

    Fixing output description and pushing in documentation.

--- aws/instance_types/outputs.tf
+++ aws/instance_types/outputs.tf
@@ -1,4 +1,5 @@
 output "instance_types" {
+  description = "Instance types in the region specified based on the instance classes requested."
   value = data.aws_ec2_instance_type_offerings.instance_types.instance_types
 }
 
